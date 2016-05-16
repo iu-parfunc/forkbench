e73a5f499e30b6607e42dcb8c522236ddb417ba5
Ryan Newton <rrnewton@gmail.com>
2016-05-16 15:16:51 -0400

    Add simple nix setup to build fib.  Problems with dynamic linking

--- hpx/default.nix
+++ hpx/default.nix
@@ -1,10 +1,11 @@
 
 { stdenv ? (import <nixpkgs> {}).stdenv
+, hpx    ? (import <nixpkgs> {}).hpx
 }:
 
 stdenv.mkDerivation {
   name = "hpx-forkbench-0.0.1"; 
   builder = ./runbench.sh; 
   src = ./. ;
-#  inherit perl; 
+  inherit hpx; 
 }
--- hpx/runbench.sh
+++ hpx/runbench.sh
@@ -6,8 +6,23 @@ echo "Inputs are here: $src"
 echo "Current dir: "`pwd`
 echo "Output results go here: $out"
 
-set -xe
+# set -xe
+set -e
 source $stdenv/setup
 
 mkdir $out
-touch $out/foo
+
+INCLUDES=" -I$hpx/include -I$hpx/lib/libffi-3.99999/include/ "
+
+set -x
+
+gcc -Wl,-rpath=$hpx/lib $INCLUDES -L$hpx/lib -lhpx $src/fibonacci.c -o $out/fib.exe
+
+# gcc $INCLUDES $hpx/lib/libhpx.so $src/fibonacci.c -o $out/fib.exe
+
+set +x
+
+echo "Here's a snapshot of the expected dynamic links:"
+ldd $out/fib.exe
+
+$out/fib.exe 10
