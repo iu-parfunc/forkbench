9b9fa774fc519b9707aa2ac7cc5c55574753c50f
Ryan Newton <rrnewton@gmail.com>
2016-05-17 09:36:19 -0400

    Write rust spawnbench

--- rust/spawnbench/src/main.rs
+++ rust/spawnbench/src/main.rs
@@ -3,9 +3,30 @@ extern crate rayon;
 
 // use std::io;
 use rayon::*;
+use std::env;
+
+fn spawntree(n : u64) -> u64 {
+    if n == 0 {
+        1 
+    } else {
+        let half1 = n / 2;
+        let half2 = half1 + (n % 2);
+        let (x,y) = join( || spawntree(half2-1),
+                          || spawntree(half1));
+        x+y
+    }
+}
 
 fn main() {
-
-    join( || println!("Hello"),
-          || println!("world!") );
+    if env::args().count() != 2 {
+        panic!("Expects exactly one argument!");
+    }
+    match env::args().last() {
+        None => panic!("Expects exactly one argument!"),
+        Some(num) => {
+            print!("spawntree({:?}) = ", num); 
+            let res = spawntree( num.parse::<u64>().unwrap() );
+            println!("{}",res);
+        }
+    }
 }
