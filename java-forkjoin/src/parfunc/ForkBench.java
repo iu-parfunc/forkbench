3174a609200f36262a493d66656e7c9c1655e1da
Michael Vollmer <vollmerm@indiana.edu>
2016-05-22 17:42:50 -0400

    Only fork once, and add optimization flags to build plan

--- java-forkjoin/src/parfunc/ForkBench.java
+++ java-forkjoin/src/parfunc/ForkBench.java
@@ -4,14 +4,13 @@ import java.util.concurrent.*;
 import java.util.*;
 
 public class ForkBench {
+
     public static void main(String[] args) {
 	try {
 	    int n = Integer.parseInt(args[0]);
 	    String readThreads = System.getenv("NUM_THREADS");
 	    int threads = Integer.parseInt(readThreads);
-	    ForkJoinPool forkJoinPool = new ForkJoinPool(threads);
-	    Bencher b = new Bencher(n);
-	    System.out.println(forkJoinPool.invoke(b));
+	    Bencher.invoke(n, threads);
 	} catch (Exception e) {
 	    System.err.println("Invalid input.");
 	    System.exit(1);
@@ -22,24 +21,33 @@ public class ForkBench {
     private static class Bencher extends RecursiveTask<Integer> {
 	private int steps;
 
-	public Bencher(int steps) {
+	public static int invoke(int n, int threads) {
+	    ForkJoinPool forkJoinPool = new ForkJoinPool(threads);
+	    Bencher b = new Bencher(n);
+	    return forkJoinPool.invoke(b);
+	}
+
+	private Bencher(int steps) {
 	    this.steps = steps;
+	}
+
+	private static int work(int steps) {
+	    if (steps == 0) {
+		return 1;
+	    } else {
+		int half1 = steps / 2;
+		int half2 = half1 + (steps % 2);
+		Bencher subtask = new Bencher(half2 - 1);
+		subtask.fork();
+		int a = work(half1);
+		int b = subtask.join();
+		return a + b;
+	    }
 	}
 
 	@Override
 	protected Integer compute() {
-	    if (steps == 0) {
-		return 1;
-	    } else {
-		int half1 = this.steps / 2;
-		int half2 = half1 + (this.steps % 2);
-		Bencher subtask1 = new Bencher(half2 - 1);
-		Bencher subtask2 = new Bencher(half1);
-		subtask1.fork();
-		subtask2.fork();
-		int ret = subtask1.join() + subtask2.join();
-		return ret;
-	    }
+	    return work(steps);
 	}
     }
 }
