7db9f7537dccf418ab87dd226ca2dae2af492de4
Ian Wang <wangyinz@156-56-64-133.dhcp-bl.indiana.edu>
2018-05-22 16:49:10 -0400

    add job as argument

--- thread_sweep.sh
+++ thread_sweep.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+JOB=$1
+
 MAX=$(cat /proc/cpuinfo | grep name | wc -l)
 
 for i in $(seq -f "%02g" 1 $MAX);
 do
-    echo    
+    echo
     echo Running with THREADS=$i;
     echo ============================================================
-    time make THREADS=$i
+    time make $JOB THREADS=$i
 done
