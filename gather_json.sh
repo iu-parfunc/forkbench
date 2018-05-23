e57f334ecce82650d45ab8e7bed30f8171910196
wangyinz <wangyinz@indiana.edu>
2018-05-23 17:01:13 -0400

    use filename

--- gather_json.sh
+++ gather_json.sh
@@ -3,6 +3,7 @@
 for i in `ls reports/*/*.json`
 do 
   echo $i | sed 's/[^0-9]*//g'
-  jq '.[2] | .[0] | .reportName' $i
+#  jq '.[2] | .[0] | .reportName' $i
+  echo ${i##*/} | sed 's/\..*//'
   jq '.[2] | .[0] | .reportAnalysis | .anRegress | .[0] | .regCoeffs | .iters | .estPoint' $i 
 done
