b322c0c51ae93e72a48ed637accd4028ab7ad4a2
wangyinz <wangyinz@indiana.edu>
2018-05-24 03:17:51 -0400

    read file

--- visualize-result/Main.hs
+++ visualize-result/Main.hs
@@ -1,10 +1,15 @@
+import System.IO
+import System.Environment
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Cairo
 
 signal :: [Double] -> [(Double,Double)]
 signal xs = [ (x,(sin (x*3.14159/45) + 1) / 2 * (sin (x*3.14159/5))) | x <- xs ]
 
-main = toFile def "example1_big.png" $ do
+main = do
+    [file] <- getArgs
+    str <- readFile file
+    toFile def "example1_big.png" $ do
     layout_title .= "Amplitude Modulation"
     setColors [opaque blue, opaque red]
     plot (line "am" [signal [0,(0.5)..400]])
